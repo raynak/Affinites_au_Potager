f8a3a0b4794265d5573e0c43af67177f0e6057ed
tanguy <tanguy.fil@gmail.com>
2015-02-08 19:01:05 +0100

    Ajout de la classe CaseComparator

---
+++ Affinites_au_Potager/src/model/CaseComparator.java
@@ -0,0 +1,24 @@
+package model;
+
+import java.util.Comparator;
+
+public class CaseComparator implements Comparator<Case> {
+
+	@Override
+	public int compare(Case c0, Case c1) {
+		if (c0.x == c1.x){
+			if (c0.y>c1.y){ return 1;}
+			else if (c0.y == c1.y){ return 0;}
+			else { return -1;}
+		}
+		else if (c0.y == c1.y){
+			if (c0.x>c1.x){ return 1;}
+			else if (c0.x == c1.x){ return 0;}
+			else { return -1;}
+		}
+		else {
+			throw new IllegalStateException();
+		}
+	}
+
+}

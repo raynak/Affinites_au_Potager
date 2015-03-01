17d98f54ad963fa58811a027c3dae0da481e9df1
Delphinep <delphine.poux@gmail.com>
2015-03-01 18:13:20 +0100

    mise en place classe affinites

--- Affinites_au_Potager/src/Tests/Main.java
+++ Affinites_au_Potager/src/Tests/Main.java
@@ -1,6 +1,20 @@
 package Tests;
 
+import java.io.File;
+
+import org.w3c.dom.*;
+
+import java.io.IOException;
 import java.util.LinkedList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 import exceptions.PlancheConstructorException;
 import model.Case;
@@ -9,20 +23,59 @@ import model.Planche;
 import model.ZonePlantation;
 
 public class Main {
-public static void main(String[] args) throws PlancheConstructorException {
-	Jardin jardin = new Jardin(10,20);
-	LinkedList<Case> cases = new LinkedList<Case>();
-	for(int i=0;i<5;i++){
-		cases.add(jardin.getTerrain()[0][i]);
+	public static void main(String[] args) throws PlancheConstructorException {
+		/*
+		 * Jardin jardin = new Jardin(10,20); LinkedList<Case> cases = new
+		 * LinkedList<Case>(); for(int i=0;i<5;i++){
+		 * cases.add(jardin.getTerrain()[0][i]); } Planche planche = new
+		 * Planche(cases); LinkedList<Planche> planches = new
+		 * LinkedList<Planche>(); planches.add(planche); ZonePlantation zone =
+		 * new ZonePlantation(); zone.setPlanches(planches);
+		 * LinkedList<ZonePlantation> zones = new LinkedList<>();
+		 * zones.add(zone); jardin.setZones(zones);
+		 */
+		// création d'une fabrique de documents
+		try {
+			DocumentBuilderFactory fabrique = DocumentBuilderFactory
+					.newInstance();
+
+			// création d'un constructeur de documents
+			DocumentBuilder constructeur = fabrique.newDocumentBuilder();
+
+			// lecture du contenu d'un fichier XML avec DOM
+			File xml = new File("data/plante2.xml");
+			Document document = constructeur.parse(xml);
+			Element racine = document.getDocumentElement();
+			//System.out.println(racine.getNodeName());
+			NodeList racineNoeuds = racine.getChildNodes();
+			int nbRacineNoeuds = racineNoeuds.getLength();
+
+			for (int i = 0; i<nbRacineNoeuds; i++) {
+			    if(racineNoeuds.item(i).getNodeType() == Node.ELEMENT_NODE) {
+			        final Element plante = (Element) racineNoeuds.item(i);
+				System.out.println(plante.getAttribute("nom"));
+				//System.out.println("ail : " + plante.getAttribute("ail"));
+				System.out.println("   affinités : ");
+				NodeList enfants = plante.getChildNodes(); 
+				int nombreDeNoeudsEnfants = enfants.getLength();
+				for(int j=1;j<nombreDeNoeudsEnfants;j+=2){
+					Node enfant = enfants.item(j);
+					String valeur = enfant.getTextContent();
+				System.out.println("   "+enfant.getNodeName() + " valeur : "+valeur);
+				}
+			    }				
+			}
+		} catch (ParserConfigurationException pce) {
+			System.out.println("Erreur de configuration du parseur DOM");
+			System.out
+					.println("lors de l'appel à fabrique.newDocumentBuilder();");
+		} catch (SAXException se) {
+			System.out.println("Erreur lors du parsing du document");
+			System.out.println("lors de l'appel à construteur.parse(xml)");
+		} catch (IOException ioe) {
+			System.out.println("Erreur d'entrée/sortie");
+			System.out.println("lors de l'appel à construteur.parse(xml)");
+		}
+
 	}
-	Planche planche = new Planche(cases);
-	LinkedList<Planche> planches = new LinkedList<Planche>();
-	planches.add(planche);
-	ZonePlantation zone = new ZonePlantation();
-	zone.setPlanches(planches);
-	LinkedList<ZonePlantation> zones = new LinkedList<>();
-	zones.add(zone);
-	jardin.setZones(zones);
-	
-}
 }
--- Affinites_au_Potager/src/model/Affinites.java
+++ Affinites_au_Potager/src/model/Affinites.java
@@ -2,6 +2,7 @@ package model;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.LinkedList;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -10,13 +11,14 @@ import javax.xml.parsers.ParserConfigurationException;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 public class Affinites {
 	private Plante plante;
 	private LinkedList<Plante> plantes;
-	private LinkedList<Plante> affinites;
-	private LinkedList<Plante> nonAffinites;
+	private HashMap<String, Integer> affinites;
 	private String fichier;
 	
 	public Affinites(Plante plante, String fichier){
@@ -26,28 +28,56 @@ public class Affinites {
 	}
 	
 	public void setAffinites(String fichier){
-		try{
-			// création d'une fabrique de documents 
-			DocumentBuilderFactory fabrique = DocumentBuilderFactory.newInstance(); 
-  
-			// création d'un constructeur de documents 
-			DocumentBuilder constructeur = fabrique.newDocumentBuilder(); 
-  
-			// lecture du contenu d'un fichier XML avec DOM 
-			File xml = new File(fichier); 
-			Document document = constructeur.parse(xml);   
-            Element racine = document.getDocumentElement();
-            System.out.println(racine);
-  
-		}catch(ParserConfigurationException pce){ 
-			System.out.println("Erreur de configuration du parseur DOM"); 
-			System.out.println("lors de l'appel à fabrique.newDocumentBuilder();"); 
-		}catch(SAXException se){ 
-			System.out.println("Erreur lors du parsing du document"); 
-			System.out.println("lors de l'appel à construteur.parse(xml)"); 
-		}catch(IOException ioe){ 
-			System.out.println("Erreur d'entrée/sortie"); 
-			System.out.println("lors de l'appel à construteur.parse(xml)"); 
-		} 
-	} 
+		// création d'une fabrique de documents
+		try {
+			DocumentBuilderFactory fabrique = DocumentBuilderFactory
+					.newInstance();
+
+			// création d'un constructeur de documents
+			DocumentBuilder constructeur = fabrique.newDocumentBuilder();
+
+			// lecture du contenu d'un fichier XML avec DOM
+			File xml = new File("data/plante2.xml");
+			Document document = constructeur.parse(xml);
+			Element racine = document.getDocumentElement();
+			//System.out.println(racine.getNodeName());
+			NodeList racineNoeuds = racine.getChildNodes();
+			int nbRacineNoeuds = racineNoeuds.getLength();
+
+			for (int i = 0; i<nbRacineNoeuds; i++) {
+			    if(racineNoeuds.item(i).getNodeType() == Node.ELEMENT_NODE) {
+			        final Element plante = (Element) racineNoeuds.item(i);
+				System.out.println(plante.getAttribute("nom"));
+				/* On cherche la bonne plante */
+				if(plante.getAttribute("nom") == this.plante.getNom()){
+					NodeList enfants = plante.getChildNodes(); 
+					int nombreDeNoeudsEnfants = enfants.getLength();
+					for(int j=1;j<nombreDeNoeudsEnfants;j+=2){
+						Node enfant = enfants.item(j);
+						this.affinites.put(enfant.getNodeName(), Integer.parseInt(enfant.getTextContent()));
+					}
+				}
+			    }				
+			}
+		} catch (ParserConfigurationException pce) {
+			System.out.println("Erreur de configuration du parseur DOM");
+			System.out
+					.println("lors de l'appel à fabrique.newDocumentBuilder();");
+		} catch (SAXException se) {
+			System.out.println("Erreur lors du parsing du document");
+			System.out.println("lors de l'appel à construteur.parse(xml)");
+		} catch (IOException ioe) {
+			System.out.println("Erreur d'entrée/sortie");
+			System.out.println("lors de l'appel à construteur.parse(xml)");
+		}
+
+	}
+	
+	public HashMap<String, Integer> getAffinites(){
+		return this.affinites;
+	}
+	
+	public int getAffinite(Plante p){
+		return this.affinites.get(p.getNom());
+	}
 }

44a173ff518c72324a0c2fefa68783ae6e41e63c
Delphinep <delphine.poux@gmail.com>
2015-03-24 22:03:36 +0100

    début algo avec contraintes

--- Affinites_au_Potager/src/model/combinatoire/ModeleCombinatoire.java
+++ Affinites_au_Potager/src/model/combinatoire/ModeleCombinatoire.java
@@ -1,5 +1,6 @@
 package model.combinatoire;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 
 import model.CaseCultivable;
@@ -66,6 +67,35 @@ public class ModeleCombinatoire extends ModeleCombi{
 	public int score() {
 		// TODO Auto-generated method stub
 		return 0;
+	}
+	
+	// Ajout des contraintes sur les plantes
+	public int getTotalAffinite(Plante p){
+		int affTotal = 0;
+		for (Plante plante : this.jardin.getPlantes()) {
+			affTotal += p.getAffinite(plante);
+		}
+		return affTotal;
+	}
+	
+	public HashMap<String,Integer> contraintesPlantes(){
+		HashMap<String,Integer> contraintesPlantes = new HashMap<String,Integer>();
+		for(Plante plante : this.jardin.getPlantes()){
+			contraintesPlantes.put(plante.getNom(), getTotalAffinite(plante));
+		}
+		return contraintesPlantes;
+	}
+	
+	public Plante getMaxContraintes(LinkedList<Plante> plantes){
+		int maxPlante = getTotalAffinite(plantes.get(0));
+		Plante p = plantes.get(0);
+		for (Plante plante : plantes) {
+			int aff = getTotalAffinite(plante);
+			if(maxPlante > maxPlante)
+				maxPlante = aff;
+				p = plante;
+		}
+		return p;
 	}
 
 }

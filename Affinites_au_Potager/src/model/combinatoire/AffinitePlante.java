36a8bedf12035d970662d9027ac8b7b05681f76f
tanguy <tanguy.fil@gmail.com>
2015-03-27 21:05:33 +0100

    Parser ok => genère correctement les HAshMap d'affinités

--- Affinites_au_Potager/src/model/combinatoire/AffinitePlante.java
+++ Affinites_au_Potager/src/model/combinatoire/AffinitePlante.java
@@ -28,13 +28,15 @@ public class AffinitePlante implements Affinites {
 	private LinkedList<String> plantes;
 	private HashMap<String, Integer> affinites;
 	private String fichier;
-	
+
 	public AffinitePlante (Plante plante, String fichier){
 		this.plante = plante;
 		this.fichier = fichier;
+		this.plantes = new LinkedList<String>();
+		this.affinites = new HashMap<String, Integer>();
 		setAffinites(fichier);
 	}
-	
+
 	public void setAffinites(String fichier){
 		// création d'une fabrique de documents
 		try {
@@ -53,26 +55,30 @@ public class AffinitePlante implements Affinites {
 			int nbRacineNoeuds = racineNoeuds.getLength();
 
 			for (int i = 0; i<nbRacineNoeuds; i++) {
-			    if(racineNoeuds.item(i).getNodeType() == Node.ELEMENT_NODE) {
-			        final Element plante = (Element) racineNoeuds.item(i);
-				System.out.println(plante.getAttribute("nom"));
-				/* On cherche la bonne plante */
-				if(plante.getAttribute("nom") == this.plante.getNom()){
-					NodeList enfants = plante.getChildNodes(); 
-					int nombreDeNoeudsEnfants = enfants.getLength();
-					for(int j=1;j<nombreDeNoeudsEnfants;j+=2){
-						Node enfant = enfants.item(j);
-						/* Avec les plantes voulues */
-						if(this.plantes.contains(enfant.getTextContent()))
-						this.affinites.put(enfant.getNodeName(), Integer.parseInt(enfant.getTextContent()));
+				if(racineNoeuds.item(i).getNodeType() == Node.ELEMENT_NODE) {
+					final Element plante = (Element) racineNoeuds.item(i);
+					System.out.println(plante.getAttribute("nom"));
+					/* On cherche la bonne plante */
+					if(plante.getAttribute("nom").equals(this.plante.getNom())){
+						NodeList enfants = plante.getChildNodes(); 
+						int nombreDeNoeudsEnfants = enfants.getLength();
+						for(int j=1;j<nombreDeNoeudsEnfants;j+=2){
+							Node enfant = enfants.item(j);
+							String nomPlante = enfant.getNodeName();
+							int valeurAffinite = Integer.parseInt(enfant.getChildNodes().item(0).getNodeValue());
+							this.plantes.addLast(nomPlante);
+							this.affinites.put(nomPlante, valeurAffinite);
+							/* Avec les plantes voulues */
+//							if(this.plantes.contains(enfant.getTextContent()))
+//								this.affinites.put(enfant.getNodeName(), Integer.parseInt(enfant.getTextContent()));
+						}
 					}
-				}
-			    }				
+				}				
 			}
 		} catch (ParserConfigurationException pce) {
 			System.out.println("Erreur de configuration du parseur DOM");
 			System.out
-					.println("lors de l'appel à fabrique.newDocumentBuilder();");
+			.println("lors de l'appel à fabrique.newDocumentBuilder();");
 		} catch (SAXException se) {
 			System.out.println("Erreur lors du parsing du document");
 			System.out.println("lors de l'appel à construteur.parse(xml)");
@@ -82,11 +88,11 @@ public class AffinitePlante implements Affinites {
 		}
 
 	}
-	
+
 	public HashMap<String, Integer> getAffinites(){
 		return this.affinites;
 	}
-	
+
 	public int getAffinite(Plante p){
 		return this.affinites.get(p.getNom());
 	}
@@ -96,9 +102,15 @@ public class AffinitePlante implements Affinites {
 		// TODO Auto-generated method stub
 		return 0;
 	}
-	
+
 	public static void main(String[] args) throws FileNotFoundException, GardenWrongDataFormatException, PlancheConstructorException, PlancheNonMitoyenneException {
 		Jardin jardin = new Jardin("data/jardin.txt");
-		
+		Plante p = new Plante("bette");
+		AffinitePlante aff = new AffinitePlante(p, "data/affinites/pante2.xml");
+		System.out.println(aff.plantes);
+		System.out.println(aff.affinites.keySet());
+		for (String s : aff.affinites.keySet()){
+			System.out.println(s+" - "+aff.affinites.get(s));
+		}
 	}
 }

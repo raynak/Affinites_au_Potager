c9037f595469bfbf683fe0e15d1baa8335f485e8
tanguy <tanguy.fil@gmail.com>
2015-04-25 22:48:48 +0200

    Listener d'affichage des noms des plantes via un tooltip

---
+++ Affinites_au_Potager/src/controler/InfoPlanteListener.java
@@ -0,0 +1,31 @@
+package controler;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+
+import view.Gui;
+
+public class InfoPlanteListener implements MouseMotionListener {
+
+	private Gui gui;
+
+	public InfoPlanteListener(Gui gui){
+		this.gui = gui;
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		int x = e.getX();
+		int y = e.getY();
+		this.gui.infoPlante(x, y);
+	}
+
+
+
+}

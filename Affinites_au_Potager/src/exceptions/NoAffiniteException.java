ee34d3df3ab71e51fa35d1111b8e5652242cfb56
tanguy <tanguy.fil@gmail.com>
2015-04-25 22:49:48 +0200

    Exception de non affinte entre 2 cases

---
+++ Affinites_au_Potager/src/exceptions/NoAffiniteException.java
@@ -0,0 +1,12 @@
+package exceptions;
+
+
+/** Classe d'exception déclenchée quand 2 cases dont au moins une ne possède pas de plante
+ * veulent connaitre l'affinite entre leur plante respective
+ * 
+ * @author Tanguy Maréchal
+ */
+@SuppressWarnings("serial")
+public class NoAffiniteException extends Exception {
+
+}
